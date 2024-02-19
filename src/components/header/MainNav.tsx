5aca642b86ff1fe60c208b9a090332ab640facad
kaiumuddin <kaium.uddin2909@gmail.com>
2024-02-19 07:48:04 +0600

    Hero section imp;emented.

---
+++ src/components/header/MainNav.tsx
@@ -0,0 +1,9 @@
+import { Button } from "../ui/button";
+
+export default function MainNav() {
+  return (
+    <Button  className="font-bold">
+      Log In
+    </Button>
+  );
+}
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,4 +1,5 @@
-import Header from "@/components/ui/Header";
+import Header from "@/components/header/Header";
+import Hero from "@/components/hero/Hero";
 import React from "react";
 
 type Props = {
@@ -9,6 +10,7 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero/>
       <div className="container mx-auto flex-1 py-10">{children}</div>
     </div>
   );
